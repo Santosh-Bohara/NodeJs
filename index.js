316694db0945cca7cc709fc2cb3799315c99541a
Santosh Bohara <sb86981@gmail.com>
2025-09-16 11:39:39 +0545

    create first file use require

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("hello world");
+
+const fs = require("fs");
+
+fs.writeFile("output.txt", "hello world", (err) => {
+  if (err) {
+    console.log("Error occurred", err);
+  } else console.log("File written successfully");
+});
