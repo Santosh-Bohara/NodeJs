9923b257c8bb00e60e58650e6a6d27308ba80117
Santosh Bohara <sb86981@gmail.com>
2025-09-17 10:31:55 +0545

    #

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-console.log("hello world");
+// console.log("hello world");
+
+// const fs = require("fs");
+
+// fs.writeFile("output.txt", "hello world", (err) => {
+//   if (err) {
+//     console.log("Error occurred", err);
+//   } else console.log("File written successfully");
+// });
 
 const fs = require("fs");
-
-fs.writeFile("output.txt", "hello world", (err) => {
+fs.writeFile("output1.txt", "hello world", (err) => {
   if (err) {
     console.log("Error occurred", err);
   } else console.log("File written successfully");
