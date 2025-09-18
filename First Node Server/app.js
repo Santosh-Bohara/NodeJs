20fece19c5fb736c4d9902f06ce1aee029701552
Santosh Bohara <sb86981@gmail.com>
2025-09-18 10:03:47 +0545

    chunk

--- First Node Server/app.js
+++ First Node Server/app.js
@@ -6,7 +6,8 @@ const fs = require("fs");
 // }
 const server = http.createServer((req, res) => {
   // console.log(req);
-  console.log(req.url, req.method, req.headers);
+  // console.log(req.url, req.method, req.headers);
+  console.log(req.url, req.method);
   // process.exit(); // Exit after logging the request
 
   if (req.url === "/") {
@@ -15,20 +16,30 @@ const server = http.createServer((req, res) => {
     res.write("<body>");
     res.write("<h1>Enter Your Details</h1>");
 
-    res.write('<form action="/submit" methosd="POST">');
+    res.write('<form action="/submit" method="POST">');
     res.write('<input type="text" name="username" placeholder="Username"><br>');
     res.write('<label for "male">Male</label>');
     res.write('<input type="radio" id="male" name="gender" value="male"/>');
     res.write('<label for "female">Female</label>');
     res.write('<input type="radio" id="female" name="gender" value="female"/>');
-    res.write("<input type='submit' value='Submit'/> <br>");
+    res.write("<input type='submit' value='submit'/> <br>");
     res.write("</form>");
 
     res.write("</body>");
 
     return res.end();
   } else if (req.url.toLocaleLowerCase === "/submit" && req.method === "POST") {
-    fs.writeFileSync("user.txt", "Santosh");
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+      console.log(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+    });
+
+    fs.writeFileSync("user.txt", "sample data");
     res.statusCode = 302;
     res.setHeader("Location", "/");
   }
@@ -38,10 +49,6 @@ const server = http.createServer((req, res) => {
 
     return res.end();
   }
-
-  res.setHeader("Content-Type", "text/html");
-  res.write("<h1>Hello from Node.js server!</h1>"); // Write response body
-  res.end(); // End the response
 });
 
 const PORT = 3000;
